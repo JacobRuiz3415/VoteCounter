bd60d619c3b01c22c9cd4b37b1697524f9ce4748
jacobruiz428 <jacobruiz428@gmail.com>
2026-07-17 21:43:40 -0700

    file

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args){
+        //create file
+        File x = new File("votes.txt");
+
         System.out.println("Hello world");
+        if(x.exists()){
+            System.out.println(x.getName());
+        }
+        else
+        {
+            System.out.println("The File does not exist");
+        }
+        
 
         int vote = 0;
 
